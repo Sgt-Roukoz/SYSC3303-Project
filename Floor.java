f4eebad482988d2a71ea976d8fbbd89450f8c883
Eric <8759230+C1erlc@users.noreply.github.com>
2024-01-31 19:42:27 -0500

    Added top/bottom floor processing

--- Floor.java
+++ Floor.java
@@ -3,22 +3,34 @@ public class Floor extends Thread{
     boolean downLampOn;
     boolean elevatorArrived;
     private final int floorNumber;
+    private final boolean isTopFloor;
+    private final boolean isBottomFloor;
 
-    public Floor(String name, int floorNumber) {
+    public Floor(String name, int floorNumber, boolean isTopFloor, boolean isBottomFloor) {
         super(name);
         this.floorNumber = floorNumber;
         this.upLampOn = false;
         this.downLampOn = false;
         this.elevatorArrived = false;
+        this.isBottomFloor = isBottomFloor;
+        this.isTopFloor = isTopFloor;
     }
 
-    public void upButtonPressed() {
+    public void upButtonPressed(int destFloor) {
+        if(isTopFloor) {
+            System.out.println("This is the top floor, there is no up button.");
+            return;
+        }
         upLampOn = true;
-        ElevatorEvent event = new ElevatorEvent(java.time.LocalTime.now().toString(), floorNumber,ELEVATOR_BUTTON.UP,1);
+        ElevatorEvent event = new ElevatorEvent(java.time.LocalTime.now().toString(), floorNumber,ELEVATOR_BUTTON.UP,destFloor);
     }
 
-    public void DownButtonPressed() {
+    public void DownButtonPressed(int destFloor) {
+        if(isBottomFloor) {
+            System.out.println("This is the bottom floor, there is no down button.");
+            return;
+        }
         downLampOn = true;
-        ElevatorEvent event = new ElevatorEvent(java.time.LocalTime.now().toString(), floorNumber,ELEVATOR_BUTTON.DOWN,1);
+        ElevatorEvent event = new ElevatorEvent(java.time.LocalTime.now().toString(), floorNumber,ELEVATOR_BUTTON.DOWN,destFloor);
     }
 }
