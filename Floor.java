514ec452d5f9fabd3e8ead98a6c9f2642b3733ab
Eric <8759230+C1erlc@users.noreply.github.com>
2024-01-31 20:47:39 -0500

    Implemented scheduler

--- Floor.java
+++ Floor.java
@@ -1,3 +1,10 @@
+/**
+ * Floor class
+ * Simulates a single floor of the building
+ *
+ * @author Eric Wang
+ * @version 2024-01-31
+ */
 public class Floor extends Thread{
     boolean upLampOn;
     boolean downLampOn;
@@ -5,9 +12,11 @@ public class Floor extends Thread{
     private final int floorNumber;
     private final boolean isTopFloor;
     private final boolean isBottomFloor;
+    private final Scheduler scheduler;
 
-    public Floor(String name, int floorNumber, boolean isTopFloor, boolean isBottomFloor) {
+    public Floor(String name, Scheduler scheduler, int floorNumber, boolean isTopFloor, boolean isBottomFloor) {
         super(name);
+        this.scheduler = scheduler;
         this.floorNumber = floorNumber;
         this.upLampOn = false;
         this.downLampOn = false;
@@ -16,6 +25,10 @@ public class Floor extends Thread{
         this.isTopFloor = isTopFloor;
     }
 
+    /**
+     * Processes given string input, sending the appropriate event to the scheduler
+     * @param input the given string input to be processed
+     */
     public void processInput(String input) {
         String[] split = input.split(" ");
         if(isTopFloor && split[2].toUpperCase() == "UP") {
@@ -29,5 +42,6 @@ public class Floor extends Thread{
         if(split[2].toUpperCase() == "UP") upLampOn = true;
         else downLampOn = true;
         ElevatorEvent event = new ElevatorEvent(split[0], Integer.valueOf(split[1]), ELEVATOR_BUTTON.valueOf(split[2].toUpperCase()), Integer.valueOf(split[3]));
+        scheduler.setEvent(event);
     }
 }
