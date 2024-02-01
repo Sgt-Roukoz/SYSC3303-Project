e9ed4300917e2650a3c86c33b2c32a4d71051d97
Marwan Zeid <45441599+Sgt-Roukoz@users.noreply.github.com>
2024-02-01 16:58:02 -0500

    Changed scheduler to allow elevator to send response.

--- Scheduler.java
+++ Scheduler.java
@@ -1,4 +1,3 @@
-import java.util.ArrayDeque;
 
 /**
  * Scheduler class
@@ -10,53 +9,83 @@ import java.util.ArrayDeque;
 
 public class Scheduler implements Runnable{
 
-    private ArrayDeque<ElevatorEvent> storedEvents;
+    private ElevatorEvent storedEvent;
+    private ElevatorEvent responseEvent;
     private boolean noEvent = true;
-    private boolean isFull = false;
-    private final int maxEvents = 5;
+    private boolean noResponseEvent = true;
 
     public Scheduler(){}
 
     @Override
     public void run() {}
 
+    /**
+     * Method holds the wait() function for synchronization purposes
+     */
+    private void mutex()
+    {
+        try {
+            wait();
+        } catch (InterruptedException ignored) {}
+    }
+
+    /**
+     * Returns the event that is currently stored in the scheduler
+     * @return Returns storedEvent as ElevatorEvent
+     */
     public synchronized ElevatorEvent getEvent()
     {
         while (noEvent)
         {
-            try {
-                wait();
-            } catch (InterruptedException ignored) {}
+            mutex();
         }
 
-        if (storedEvents.size() < maxEvents)
-        {
-            isFull = false;
-        }
-        if (storedEvents.isEmpty())
-        {
-            noEvent = true;
-        }
-        notifyAll();
-        return storedEvents.poll();
+        noEvent = true;
+        return storedEvent;
     }
 
+    /**
+     * Saves the event in the scheduler.
+     *
+     * @param event The event to be stored
+     */
     public synchronized void setEvent(ElevatorEvent event)
     {
-        while (!noEvent && !isFull)
+        while (!noEvent)
         {
-            try {
-                wait();
-            } catch (InterruptedException ignored) {}
+            mutex();
         }
 
-        storedEvents.add(event);
-        if (storedEvents.size() >= maxEvents)
-        {
-            isFull = true;
-        }
+        storedEvent = event;
         noEvent = false;
         notifyAll();
+    }
+
+    /**
+     * Saves a response event to the scheduler
+     *
+     * @param event The event to be stored
+     */
+    public synchronized void setResponseEvent(ElevatorEvent event)
+    {
+        responseEvent = event;
+        noResponseEvent = false;
+        notifyAll();
+    }
+
+    /**
+     * Returns the response event currently stored by the scheduler
+     * @return Returns the current response
+     */
+    public synchronized ElevatorEvent getResponseEvent()
+    {
+        while(noResponseEvent)
+        {
+            mutex();
+        }
+
+        noResponseEvent = true;
+        return responseEvent;
     }
 
 
