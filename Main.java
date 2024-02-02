e3fc1b56ee27ad2179670a326101c0d8af19f7b6
adham <adham777ad@gmail.com>
2024-02-02 18:30:44 -0500

    Added Elevator class and modified ELEVATOR_BUTTON

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         EventQueue eventQueue = new EventQueue();
         Scheduler scheduler = new Scheduler(eventQueue);
-        Elevator elevator = new Elevator(scheduler, eventQueue, 1);
+        Elevator elevator = new Elevator(eventQueue, 1);
         Floor floor = new Floor("Floor 1", eventQueue, 1, false, true);
 
         Thread floorThread = new Thread(floor);
