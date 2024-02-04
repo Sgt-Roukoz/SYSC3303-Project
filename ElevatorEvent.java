3636398979dfcab5a3d38061cd7d313866460d03
Ju3dgement <94979802+Ju3dgement@users.noreply.github.com>
2024-02-03 19:09:49 -0500

    Update ElevatorEvent.java

--- ElevatorEvent.java
+++ ElevatorEvent.java
@@ -1,46 +1,39 @@
-/**
- * ElevatorEvent class
- * encapsulates information required for an elevator subsystem to know its next course of action
- *
- * @author Marwan Zeid
- * @version 2024-01-30
- */
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
-public class ElevatorEvent { //convert to Record?
+public class ElevatorEvent {
     private final String time;
     private final int floor;
     private final ELEVATOR_BUTTON button;
     private final int car_button;
 
-    //later add value for specific elevator
-
-    public ElevatorEvent(String time, int floor, ELEVATOR_BUTTON button, int car_button)
-    {
+    public ElevatorEvent(String time, int floor, ELEVATOR_BUTTON button, int car_button) {
         this.time = time;
         this.floor = floor;
         this.button = button;
         this.car_button = car_button;
     }
 
-    public String toString(){
-        return time + " " + floor + " " + button + " " + car_button;
+    public String toString() {
+        String var10000 = this.time;
+        return var10000 + " " + this.floor + " " + String.valueOf(this.button) + " " + this.car_button;
     }
 
-    // Getters for each of the values
-    public String getTime()
-    {
-        return time;
+    public String getTime() {
+        return this.time;
     }
 
     public int getFloor() {
-        return floor;
+        return this.floor;
     }
 
     public ELEVATOR_BUTTON getButton() {
-        return button;
+        return this.button;
     }
 
     public int getCar_button() {
-        return car_button;
+        return this.car_button;
     }
 }
