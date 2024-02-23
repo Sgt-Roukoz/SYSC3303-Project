1fb213e70d22199d754ae4a96c3bd1ec1678ca78
Ju3dgement <94979802+Ju3dgement@users.noreply.github.com>
2024-02-22 19:42:31 -0500

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -2,35 +2,64 @@
  * Scheduler class
  * Acts as a communication channel for the floor and elevator subsystems
  *
- * @author Marwan Zeid
- * @version 2024-02-02
+ * @author Garrison Su
+ * @version 2024-02-22
  */
 
-public class Scheduler implements Runnable{
-
+public class Scheduler implements Runnable {
+    private enum SchedulerState {
+        IDLE,
+        PROCESSING_COMMAND,
+        WAITING
+    }
+    private SchedulerState state;
     private final EventQueue eventQueue;
     private ElevatorEvent floorRequestToBeProcessed;
     private ElevatorEvent processedRequest;
 
     /**
      * Scheduler class constructor
+     *
      * @param eventQueue EventQueue object where various events will be stored
      */
-    public Scheduler(EventQueue eventQueue){
+    public Scheduler(EventQueue eventQueue) {
         this.eventQueue = eventQueue;
+        this.state = SchedulerState.IDLE; // starting state
     }
 
-    /**
-     * Main thread loop
-     */
+//    @Override
+//    public void run() {
+//        while (eventQueue.processedEvents < eventQueue.maxEvents) {
+//
+//            readFloorRequest();
+//            if (floorRequestToBeProcessed != null) {
+//                processFloorRequest();
+//                sendElevatorRequest();
+//            }
+//        }
+//    }
+
     @Override
     public void run() {
         while (eventQueue.processedEvents < eventQueue.maxEvents) {
-
-            readFloorRequest();
-            if (floorRequestToBeProcessed != null) {
-                processFloorRequest();
-                sendElevatorRequest();
+            switch (state) {
+                case IDLE:
+                    readFloorRequest();
+                    if (floorRequestToBeProcessed != null) {
+                        System.out.println("Scheduler State: " + floorRequestToBeProcessed + ": Idle");
+                        state = SchedulerState.PROCESSING_COMMAND;
+                    }
+                    break;
+                case PROCESSING_COMMAND:
+                    System.out.println("Scheduler State: PROCESSING_COMMAND");
+                    processFloorRequest();
+                    sendElevatorRequest();
+                    state = SchedulerState.WAITING;
+                    break;
+                case WAITING:
+                    System.out.println("Scheduler State: WAITING");
+                    state = SchedulerState.IDLE;
+                    break;
             }
         }
     }
@@ -38,16 +67,14 @@ public class Scheduler implements Runnable{
     /**
      * Read an ElevatorEvent from the floor request queue in eventQueue
      */
-    private void readFloorRequest()
-    {
+    private void readFloorRequest() {
         floorRequestToBeProcessed = eventQueue.getFloorRequest();
     }
 
     /**
      * Process the ElevatorEvent request and convert into an event usable by Elevator
      */
-    private void processFloorRequest()
-    {
+    private void processFloorRequest() {
         //do stuff, functionality ot be added in later iteration
 
         System.out.println("Scheduler: Processing floor event: " + floorRequestToBeProcessed);
@@ -57,10 +84,10 @@ public class Scheduler implements Runnable{
     /**
      * Send the processed ElevatorEvent to the elevator queue in eventQueue
      */
-    private void sendElevatorRequest()
-    {
-            System.out.println("Sending elevator event: " + processedRequest);
-            eventQueue.setElevatorRequest(processedRequest);
+    private void sendElevatorRequest() {
+        System.out.println("Sending elevator event: " + processedRequest);
+        eventQueue.setElevatorRequest(processedRequest);
+        // eventQueue.processedEvents++;
     }
 
     public void setReadFloorRequest() {
@@ -86,4 +113,23 @@ public class Scheduler implements Runnable{
     public ElevatorEvent getProcessedRequest() {
         return this.processedRequest;
     }
+
+
+
+    // public static void main(String[] args) {
+    //     EventQueue eventQueue = new EventQueue();
+
+    //     // Simulate adding floor requests to the event queue
+    //     ElevatorEvent floorRequest = new ElevatorEvent("14:05:15.0", 2, ELEVATOR_BUTTON.UP, 4);
+    //     eventQueue.setFloorRequest(floorRequest);
+    //     ElevatorEvent floorRequest2 = new ElevatorEvent("17:05:20.0", 3, ELEVATOR_BUTTON.DOWN, 5);
+    //     eventQueue.setFloorRequest(floorRequest2);
+    //     ElevatorEvent floorRequest3 = new ElevatorEvent("18:05:15.0", 1, ELEVATOR_BUTTON.UP, 6);
+    //     eventQueue.setFloorRequest(floorRequest3);
+
+    //     Scheduler scheduler = new Scheduler(eventQueue);
+    //     scheduler.run();
+    //     System.out.println("All events finished!");
+    // }
 }
+
