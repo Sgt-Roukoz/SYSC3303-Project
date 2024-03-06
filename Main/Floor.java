69c3b21152eb4b955b5acdae61c53dc629e874dd
Eric <8759230+C1erlc@users.noreply.github.com>
2024-03-05 23:51:22 -0500

    basic setup for UDP

--- Main/Floor.java
+++ Main/Floor.java
@@ -2,29 +2,36 @@ package Main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Floor class
- * Simulates a single floor of the building, including lamps and buttons
+ * Simulates all floors of the building, including lamps and buttons
  *
  * @author Eric Wang
- * @version 2024-01-31
+ * @version 2024-03-05
  */
 public class Floor extends Thread{
-    boolean upLampOn;
-    boolean downLampOn;
-    boolean elevatorArrived;
     private final EventQueue eventQueue;
-
+    private final int MAX_FLOORS = 22;
+    private final Random rand;
+    private final DatagramSocket socket;
+    private final DatagramPacket packet;
 
     public Floor(String name, EventQueue eventQueue) {
         super(name);
         this.eventQueue = eventQueue;
-        this.upLampOn = false;
-        this.downLampOn = false;
-        this.elevatorArrived = false;
-
+        this.rand = new Random();
+        this.packet = new DatagramPacket(new byte[100], 100);
+        try {
+            this.socket = new DatagramSocket();
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -37,9 +44,12 @@ public class Floor extends Thread{
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 this.processInput(line);
+                Thread.sleep(rand.nextInt(500,2000));
             }
             scanner.close();
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
@@ -50,19 +60,13 @@ public class Floor extends Thread{
      */
     public void processInput(String input) {
         String[] split = input.split(" ");
-        /*if(isTopFloor && split[2].equalsIgnoreCase("UP")) {
-            System.out.println("This is the top floor, there is no up button.");
-            return;
-        }
-        else if (isBottomFloor && split[2].equalsIgnoreCase("DOWN")) {
-            System.out.println("This is the bottom floor, there is no down button.");
-            return;
-        }*/
-        if(split[2].equalsIgnoreCase("UP")) upLampOn = true;
-        else downLampOn = true;
-        ElevatorEvent event = new ElevatorEvent(split[0], Integer.valueOf(split[1]), ELEVATOR_BUTTON.valueOf(split[2].toUpperCase()), Integer.valueOf(split[3]));
-        System.out.println("Floor adding event: " + event );
-        eventQueue.setFloorRequest(event);
+        if(split[2].equalsIgnoreCase("UP")) System.out.println("Floor " + split[3] + " up lamp on");
+        else System.out.println("Floor " + split[3] + " down lamp on");
+        byte[] msg = new byte[100];
+        //TODO: send UDP
+        /*ElevatorEvent event = new ElevatorEvent(split[0], Integer.valueOf(split[1]), ELEVATOR_BUTTON.valueOf(split[2].toUpperCase()), Integer.valueOf(split[3]));
+        System.out.println("Floor sending event: " + event );
+        eventQueue.setFloorRequest(event);*/
     }
 
     /**
